06144709914f86f5171656a812c9663ffd0281df
Bryan <bryanhh.lb@gmail.com>
2025-04-27 16:09:18 -0700

    DriveSubsystem defining in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,12 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
+
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
 
   public RobotContainer() {
     configureBindings();
