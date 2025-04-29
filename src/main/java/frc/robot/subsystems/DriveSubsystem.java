54dbcac4f55d9de5d709c73cfa719dff627d507b
Jordan Powers <jordanpowers1227@gmail.com>
2025-04-28 21:43:48 -0700

    Revert "Swerve drive yagsl"

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,50 +1,48 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meter;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.core.CoreCANcoder;
+import com.ctre.phoenix6.hardware.core.CoreTalonFX;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import java.io.File;
-import java.util.function.Supplier;
-import swervelib.SwerveDrive;
-import swervelib.parser.SwerveParser;
 
-public class DriveSubsystem extends SubsystemBase {
+public class DriveSubsystem extends SubsystemBase{
+    private final TalonFX driveMotor;
+    private final TalonFX steerMotor;
+    private final CANcoder moduleEncoder;
 
-    File directory = new File(Filesystem.getDeployDirectory(), "swerve");
-    SwerveDrive swerveDrive;
+    public static final Distance SWERVE_WHEEL_OFFSET = Units.Inch.of(11.5);
 
-    public DriveSubsystem() {
-        try {
-            swerveDrive = new SwerveParser(directory)
-                    .createSwerveDrive(
-                            Constants.maximumSpeed,
-                            new Pose2d(new Translation2d(Meter.of(1), Meter.of(4)), Rotation2d.fromDegrees(0)));
-            // Alternative method if you don't want to supply the conversion factor via JSON files.
-            // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor,
-            // driveConversionFactor);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    public SwerveDrive getSwerveDrive() {
-        return swerveDrive;
-    }
+    public DriveSubsystem(int driveMotorCANID, int steerMotorCANID, int cancoderCANID) {
 
-    public void driveFieldOriented(ChassisSpeeds velocity) {
-        swerveDrive.driveFieldOriented(velocity);
-    }
 
-    public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
-        return run(() -> {
-            swerveDrive.driveFieldOriented(velocity.get());
-        });
-    }
+        driveMotor = new TalonFX(driveMotorCANID);
+        steerMotor = new TalonFX(steerMotorCANID);
+
+        moduleEncoder = new CANcoder(cancoderCANID);
+        
+        Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+        Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+        Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+        Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+
+        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+        );
+
+        ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
+        SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
+        SwerveModuleState frontLeft = moduleStates[0];
+        SwerveModuleState frontRight = moduleStates[1];
+        SwerveModuleState backLeft = moduleStates[2];
+        SwerveModuleState backRight = moduleStates[3];
+    }    
 }
