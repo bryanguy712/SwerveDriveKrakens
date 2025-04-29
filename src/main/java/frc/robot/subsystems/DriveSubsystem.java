5bbe3a47931aec8aae3be9824d4020c30fbfa7e5
Bryan <bryanhh.lb@gmail.com>
2025-04-28 21:51:24 -0700

    Updated Pose2d

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,10 +1,6 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meter;
-
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -22,13 +18,7 @@ public class DriveSubsystem extends SubsystemBase {
 
     public DriveSubsystem() {
         try {
-            swerveDrive = new SwerveParser(directory)
-                    .createSwerveDrive(
-                            Constants.maximumSpeed,
-                            new Pose2d(new Translation2d(Meter.of(1), Meter.of(4)), Rotation2d.fromDegrees(0)));
-            // Alternative method if you don't want to supply the conversion factor via JSON files.
-            // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor,
-            // driveConversionFactor);
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maximumSpeed, Pose2d.kZero);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
