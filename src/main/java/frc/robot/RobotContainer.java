b4c52703faa27726c969a8c2b9cb6309a2d66b1f
bryanguy712 <bryanhh.lb@gmail.com>
2025-09-28 12:55:32 -0700

    Binded Auto Align to buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AutoAlign;
 import frc.robot.subsystems.DriveSubsystem;
 import swervelib.SwerveInputStream;
 
@@ -35,6 +36,13 @@ public class RobotContainer {
     public RobotContainer() {
         configureBindings();
         driveBase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+
+        while (driveController.getXButton()) {
+            new AutoAlign(false, driveBase).withTimeout(3);
+        }
+        while (driveController.getAButton()) {
+            new AutoAlign(true, driveBase).withTimeout(3);
+        }
     }
 
     private void configureBindings() {
