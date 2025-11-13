09011de29b254a583dd1f4786e401786a3a2bf0a
Momentum Robotics <momentumrobotics@gmail.com>
2025-11-12 19:57:16 -0800

    Variables changed to MoPrefs

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -1,12 +1,18 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.component.LimelightHelpers;
+import frc.robot.moprefs.MoPrefs;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoAlign extends Command {
@@ -14,60 +20,119 @@ public class AutoAlign extends Command {
     private boolean scoreRightSide;
     private Timer dontSeeTagTimer, stopTimer;
     private DriveSubsystem drive;
-    private double tagID = 0;
+    private int tagID = 0;
+
+    private final DoublePublisher xPos;
+    private final DoublePublisher yPos;
+    private final DoublePublisher rotPos;
+    private final DoublePublisher xOutput;
+    private final DoublePublisher yOutput;
+    private final DoublePublisher rotOutput;
+    private final DoublePublisher dontSeeTagTimerPublisher;
+    private final DoublePublisher stopTimerPublisher;
+    private final IntegerPublisher tagIdPublisher;
 
     public AutoAlign(boolean scoreRightSide, DriveSubsystem drive) {
-        xPIDController = new PIDController(Constants.X_REEF_ALIGNMENT_P, 0.0, 0); // Vertical movement
-        yPIDController = new PIDController(Constants.Y_REEF_ALIGNMENT_P, 0.0, 0); // Horitontal movement
-        rotationPIDController = new PIDController(Constants.ROT_REEF_ALIGNMENT_P, 0, 0); // Rotation
+        xPIDController = new PIDController(MoPrefs.autoAlignXP.get(), 0.0, 0); // Vertical movement
+        MoPrefs.autoAlignXP.subscribe(kP -> {
+            xPIDController.setP(kP);
+        });
+        
+        yPIDController = new PIDController(MoPrefs.autoAlignYP.get(), 0.0, 0); // Horitontal movement
+        MoPrefs.autoAlignYP.subscribe(kP -> {
+            yPIDController.setP(kP);
+        }); 
+
+        rotationPIDController = new PIDController(MoPrefs.autoAlignRoationP.get(), 0, 0); // Rotation
+        MoPrefs.autoAlignRoationP.subscribe(kp -> {
+            rotationPIDController.setP(kp);
+        });
+
+
+        MoPrefs.autoAlignXTolerance.subscribe(tolerance -> {
+            xPIDController.setTolerance(tolerance);
+        }, true);
+
+        MoPrefs.autoAlignYTolerance.subscribe(tolerance -> {
+            yPIDController.setTolerance(scoreRightSide ? tolerance : -tolerance);
+        }, true);
+
+        MoPrefs.autoAlignRotTolerance.subscribe(tolerance -> {
+            rotationPIDController.setTolerance(tolerance);
+        }, true);
+
         this.scoreRightSide = scoreRightSide;
         this.drive = drive;
+        
+        this.stopTimer = new Timer();
+        this.dontSeeTagTimer = new Timer();
+
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("Auto Align");
+
+        xPos = table.getDoubleTopic("xPos").publish();
+        yPos = table.getDoubleTopic("yPos").publish();
+        rotPos = table.getDoubleTopic("rotPos").publish();
+        xOutput = table.getDoubleTopic("xOutput").publish();
+        yOutput = table.getDoubleTopic("yOutput").publish();
+        rotOutput = table.getDoubleTopic("rotOutput").publish();
+        dontSeeTagTimerPublisher = table.getDoubleTopic("dontSeeTagTimer").publish();
+        stopTimerPublisher = table.getDoubleTopic("stopTimer").publish();
+        tagIdPublisher = table.getIntegerTopic("tagId").publish();
+
         addRequirements(drive);
     }
 
     @Override
     public void initialize() {
-        this.stopTimer = new Timer();
-        this.stopTimer.start();
-        this.dontSeeTagTimer = new Timer();
-        this.dontSeeTagTimer.start();
+        this.stopTimer.restart();
+        this.dontSeeTagTimer.restart();
 
-        xPIDController.setSetpoint(Constants.X_SETPOINT_REEF_ALIGNMENT);
-        xPIDController.setTolerance(Constants.X_TOLERANCE_REEF_ALIGNMENT);
+        xPIDController.setSetpoint(scoreRightSide? -MoPrefs.autoAlignXSetPoint.get() : MoPrefs.autoAlignXSetPoint.get());
 
-        yPIDController.setSetpoint(
-                scoreRightSide ? Constants.Y_SETPOINT_REEF_ALIGNMENT : -Constants.Y_SETPOINT_REEF_ALIGNMENT);
-        yPIDController.setTolerance(Constants.Y_TOLERANCE_REEF_ALIGNMENT);
+        yPIDController.setSetpoint(MoPrefs.autoAlignYSetpoint.get());
 
-        rotationPIDController.setSetpoint(Constants.ROT_SETPOINT_REEF_ALIGNMENT);
-        rotationPIDController.setTolerance(Constants.ROT_TOLERANCE_REEF_ALIGNMENT);
+        rotationPIDController.setSetpoint(MoPrefs.autoAlignRotSetpoint.get());
 
-        tagID = LimelightHelpers.getFiducialID("");
+        tagID = (int) LimelightHelpers.getFiducialID(Constants.LIMELIGHT_NAME);
     }
 
     @Override
     public void execute() {
-        if (LimelightHelpers.getTV("") && LimelightHelpers.getFiducialID("") == tagID) {
+        if (LimelightHelpers.getTV(Constants.LIMELIGHT_NAME) && ((int)LimelightHelpers.getFiducialID(Constants.LIMELIGHT_NAME)) == tagID) {
             this.dontSeeTagTimer.reset();
 
-            double[] postions = LimelightHelpers.getBotPose_TargetSpace("");
-            SmartDashboard.putNumber("x", postions[2]);
+            double[] positions = LimelightHelpers.getBotPose_TargetSpace(Constants.LIMELIGHT_NAME);
+            double x = positions[0];
+            double y = positions[2];
+            double rot = positions[4];
 
-            double xSpeed = xPIDController.calculate(postions[2]);
-            SmartDashboard.putNumber("xspee", xSpeed);
-            double ySpeed = -yPIDController.calculate(postions[0]);
-            double rotValue = -rotationPIDController.calculate(postions[4]);
+            double xSpeed = -xPIDController.calculate(x);
+            double ySpeed = yPIDController.calculate(y);
+            double rotValue = -rotationPIDController.calculate(rot);
 
-            drive.drive(new Translation2d(xSpeed, ySpeed), rotValue, false);
+            xSpeed = MathUtil.clamp(xSpeed, -MoPrefs.autoAlignMaxSpeed.get(), MoPrefs.autoAlignMaxSpeed.get());
+            ySpeed = MathUtil.clamp(ySpeed, -MoPrefs.autoAlignMaxSpeed.get(), MoPrefs.autoAlignMaxSpeed.get());
+
+            xPos.accept(x);
+            yPos.accept(y);
+            rotPos.accept(rot);
+            xOutput.accept(xSpeed);
+            yOutput.accept(ySpeed);
+            rotOutput.accept(rotValue);
+
+            drive.drive(new Translation2d(ySpeed, xSpeed), rotValue, false);
 
             if (!rotationPIDController.atSetpoint() || !yPIDController.atSetpoint() || !xPIDController.atSetpoint()) {
                 stopTimer.reset();
             }
         } else {
             drive.drive(new Translation2d(), 0, false);
+            stopTimer.reset();
         }
 
-        SmartDashboard.putNumber("poseValidTimer", stopTimer.get());
+        dontSeeTagTimerPublisher.accept(dontSeeTagTimer.get());
+        stopTimerPublisher.accept(stopTimer.get());
+        tagIdPublisher.accept(tagID);
     }
 
     @Override
